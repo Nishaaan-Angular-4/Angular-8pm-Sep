fab4ae21a2c57d6ebb009731a4f29b3bdb1d202e
kiranpvs <trainer.paturi@gmail.com>
2016-10-20 21:03:36 +0530

    added code

--- AngularProject/app/header/headerCtrl.js
+++ AngularProject/app/header/headerCtrl.js
@@ -1,7 +1,28 @@
 (function () {
     function headerCtrlFn($scope) {
+        $scope.brandName = "kiran";
+        /*$scope.navItems = ["Home", "Profile", "Products", "Cart", "Register", "Login"];*/
+        $scope.navItems = [{
+            name: "Home",
+            templateUrl: "app/home/home.html"
+        }, {
+            name: "Products",
+            templateUrl: "app/products/products.html"
+        }, {
+            name: "Cart",
+            templateUrl: "app/cart/cart.html"
+        }, {
+            name: "Register",
+            templateUrl: "app/register/register.html"
+        }, {
+            name: "Login",
+            templateUrl: "app/login/login.html"
+        }];
 
+        $scope.loadPage = function (data) {
+            console.log(data);
+            $scope.contentUrl = data.templateUrl;
+        };
     };
-    angular.module("header")
-        .controller("headerCtrl", headerCtrlFn);
+    angular.module("header").controller("headerCtrl",headerCtrlFn);
 })();

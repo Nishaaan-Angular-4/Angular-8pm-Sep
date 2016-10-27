4954d034d8162c12d2d3d8b0fbcf0a3f865b3362
kiranuidev <trainer.paturi@gmail.com>
2016-10-27 20:57:23 +0530

    added filters

--- AngularProject/app/products/productCtrl.js
+++ AngularProject/app/products/productCtrl.js
@@ -1,6 +1,21 @@
 (function () {
     function productCtrl($scope, productSvc) {
-        $scope.products = productSvc.getProducts();
+        $scope.yearSort="Year";
+        //$scope.products = productSvc.getProducts();
+        productSvc.getProductsByPromise()
+            .then(function (response) {
+                console.log(response);
+                $scope.vehicles = response.data.vehicles;
+            })
+            .catch(function (errorResponse) {
+                console.log(errorResponse);
+            });
+        console.log("i am executing first");
+        
+        $scope.sortByYear = function(){
+            $scope.yearSort= $scope.yearSort=="Year"?"-Year":"Year";
+        };
+
     }
     angular.module("product")
         .controller("productCtrl", ["$scope", "productSvc", productCtrl])
--- AngularProject/app/register/registerCtrl.js
+++ AngularProject/app/register/registerCtrl.js
@@ -7,12 +7,21 @@
         $scope.registerUser = function () {
             console.log($scope.user);
         };
-        $scope.countryList = lookupSvc.getCountryList();
+        //$scope.countryList = lookupSvc.getCountryList();
 
         $scope.loadStateList = function () {
             var countryCode = $scope.user.country.code;
             $scope.stateList = lookupSvc.getStateListByCountry(countryCode);
         };
+
+        lookupSvc.getCountriesFromApi()
+            .then(function (response) {
+                console.log(response);
+                $scope.countryList = response.data.countries;
+            })
+            .catch(function (err) {
+                console.log(err);
+            });
     }
 
     angular.module("register").controller("registerCtrl", registerCtrl);
